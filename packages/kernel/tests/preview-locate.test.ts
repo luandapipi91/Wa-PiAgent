9402be02436510232cf4b9deb54456b5d8349e34
co <luandapipi@126.com>
2026-08-21 23:40:55 +0800

    fix: preview-locate 段规则与前端 buildSelector 统一，body 不特判改用 nth-of-type

--- packages/kernel/tests/preview-locate.test.ts
+++ packages/kernel/tests/preview-locate.test.ts
@@ -16,33 +16,33 @@ const HTML = [
 ].join("\n");
 
 test("按 id 定位元素及闭合行", () => {
-	expect(locateElement(HTML, "html > body > div#card")).toEqual({
+	expect(locateElement(HTML, "html > body:nth-of-type(1) > div#card")).toEqual({
 		startLine: 5,
 		endLine: 8,
 	});
 });
 
 test("按 nth-of-type 定位兄弟元素", () => {
-	expect(locateElement(HTML, "html > body > div#card > p:nth-of-type(2)")).toEqual({
+	expect(locateElement(HTML, "html > body:nth-of-type(1) > div#card > p:nth-of-type(2)")).toEqual({
 		startLine: 7,
 		endLine: 7,
 	});
 });
 
 test("无 id 元素用 nth-of-type 段", () => {
-	expect(locateElement(HTML, "html > body > div:nth-of-type(2) > span:nth-of-type(1)")).toEqual({
+	expect(locateElement(HTML, "html > body:nth-of-type(1) > div:nth-of-type(2) > span:nth-of-type(1)")).toEqual({
 		startLine: 9,
 		endLine: 9,
 	});
 });
 
 test("selector 不匹配返回 null", () => {
-	expect(locateElement(HTML, "html > body > ul:nth-of-type(1)")).toBeNull();
+	expect(locateElement(HTML, "html > body:nth-of-type(1) > ul:nth-of-type(1)")).toBeNull();
 });
 
 test("script 内容里的假标签不干扰", () => {
 	const html = '<html>\n<body>\n<script>var s = "<div>fake</div>";</script>\n<div id="real">x</div>\n</body>\n</html>';
-	expect(locateElement(html, "html > body > div#real")).toEqual({
+	expect(locateElement(html, "html > body:nth-of-type(1) > div#real")).toEqual({
 		startLine: 4,
 		endLine: 4,
 	});
@@ -50,7 +50,7 @@ test("script 内容里的假标签不干扰", () => {
 
 test("void 元素（img）自身起止同行", () => {
 	const html = '<html>\n<body>\n<img src="a.png">\n</body>\n</html>';
-	expect(locateElement(html, "html > body > img:nth-of-type(1)")).toEqual({
+	expect(locateElement(html, "html > body:nth-of-type(1) > img:nth-of-type(1)")).toEqual({
 		startLine: 3,
 		endLine: 3,
 	});
